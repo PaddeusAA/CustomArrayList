9948eadb28b57745debadedd19ea68e6da14dd73
VladislavPaddeus <paddeusvladislav@gmail.com>
2023-08-20 00:59:04 +0400

    Добавил новые функции

--- src/main/java/CustomArrayList.java
+++ src/main/java/CustomArrayList.java
@@ -1,6 +1,8 @@
+
+//Пока работает только со String
 public class CustomArrayList {
-    private int size;
-    private int capacity;
+    private int size; // Отвечает за динамическое расширение массива
+    private int capacity; // Размер массива
     private String[] elements;
 
     public CustomArrayList() {
@@ -17,6 +19,31 @@ public class CustomArrayList {
         size++;
     }
 
+    public void remove(int elementIndex){
+        for (int i = 2; i < elements.length; i++) {
+            //выполняем перемещение элементов к началу, в результате чего пустая ячейка окажется в конце
+            elements[i-1] = elements[i];
+            elements[i] = null;
+        }
+        size--;
+    }
+
+    public void print(){
+        for(String str : elements){
+            if(str != null){
+                System.out.println(str);
+            }
+        }
+    }
+
+    public void clear(){
+        for (int i = 0; i < elements.length; i++){
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    // Метод автоматически вызывается когда заканчиваются свободные ячейки
     private void grow() {
         capacity = (int) (capacity * 1.5);
         String[] tempElements = new String[capacity];
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        CustomArrayList customArrayList = new CustomArrayList();
 
-        customArrayList.add("Array");
+
     }
 
 }
